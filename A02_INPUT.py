cf5ea2274fd871a2582513a48b202a68e45522e3
LucassTadeu <06luvitorio09@gmail.com>
2024-09-24 09:02:16 -0300

    .

---
+++ A02_INPUT.py
@@ -0,0 +1,13 @@
+Nome = "Asdrubal"
+nome = input("Digite seu nome")
+
+print(nome)
+n1 = int(input("Digite um número inteiro: "))
+n2 = input("Digite outro número inteiro: ")
+n2 = int(n2)
+print(n1+n2)
+
+Sobrenome = input("Digite seu sobrenome: ")
+Idade = input("Digite sua idade: ")
+print(Sobrenome + Idade)
+print(f"Seu nome é {nome} {Sobrenome} e sua idade é {Idade}")
